804bee61b024fba47f019b8d0300b5501596e924
Ali <theali9000@gmail.com>
2024-12-14 19:15:20 +0400

    Initial commit

---
+++ src/main/java/net/nubnik/infinitefusion/InfiniteFusion.java
@@ -0,0 +1,16 @@
+package net.nubnik.infinitefusion;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InfiniteFusion implements ModInitializer {
+	public static final String MOD_ID = "infinitefusion";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/nubnik/infinitefusion/InfiniteFusionClient.java
@@ -0,0 +1,10 @@
+package net.nubnik.infinitefusion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InfiniteFusionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
